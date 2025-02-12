ca76cf96b7272e22204e13dc32f35f1623288a2f
sagarjain03 <141217003+sagarjain03@users.noreply.github.com>
2025-02-12 18:37:08 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+const page = () => {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+      <Button>Dabao na</Button>
+    </div>
+  )
+}
+
+export default page
